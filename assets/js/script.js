9424b089d654deaade240ccaeae4db90d96e6429
light <light2adrifter@yahoo.com>
2021-11-03 12:17:10 -0400

    added type to the form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
+    
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task.";
+    
+    // create div to hold task infor and add list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info"
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 }
 
